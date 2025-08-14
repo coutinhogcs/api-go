a35b6eec89d967f663e73cd23975132ecefcd68e
Guilherme Coutinho <guilhermecoutslc@outlook.com>
2025-08-13 21:54:24 -0300

    feat: adicionando o update na api, debug e finalizando.

--- controllers/index.go
+++ controllers/index.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"api-go/models"
+	"log"
 	"net/http"
 	"strconv"
 	"text/template"
@@ -37,11 +38,44 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 		}
 		models.CriaNovoProduto(nome, descricao, precoConvertido, quantidadeConvertido)
 	}
-	http.Redirect(w, r, "/", 301)
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	models.DeletaProduto(id)
-	http.Redirect(w, r, "/", 301)
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	produto := models.EditaProduto(id)
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "Post" {
+		idstr := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		precoStr := r.FormValue("preco")
+		quantidadeStr := r.FormValue("quantidade")
+
+		id, err := strconv.Atoi(idstr)
+		if err != nil {
+			log.Println("Erro na convesão do ID para int:", err)
+		}
+
+		preco, err := strconv.ParseFloat(precoStr, 64)
+		if err != nil {
+			log.Println("Erro na convesão do preço para float64:", err)
+		}
+		quantidade, err := strconv.Atoi(quantidadeStr)
+		if err != nil {
+			log.Println("Erro na convesão da quantidade para int:", err)
+		}
+		models.AtualizaProduto(id, nome, descricao, preco, quantidade)
+
+	}
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
--- models/produtos.go
+++ models/produtos.go
@@ -55,7 +55,7 @@ func CriaNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	}
 
 	insert.Exec(nome, descricao, preco, quantidade)
-	db.Close()
+	defer db.Close()
 }
 
 func DeletaProduto(id string) {
@@ -67,5 +67,49 @@ func DeletaProduto(id string) {
 	}
 
 	delete.Exec(id)
-	db.Close()
+	defer db.Close()
+}
+
+func EditaProduto(id string) Produto {
+	db := db.ConnDb()
+
+	produtoSalvo, err := db.Query("select * from produtos where id = $1", id)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	p := Produto{}
+
+	for produtoSalvo.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = produtoSalvo.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+	}
+
+	defer db.Close()
+	return p
+}
+
+func AtualizaProduto(id int, nome, descricao string, preco float64, quantidade int) {
+	db := db.ConnDb()
+
+	AtualizaProduto, err := db.Prepare("update produtos set nome=$1, descricao=$2, preco=$3, quantidade=$4 where id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	AtualizaProduto.Exec(nome, descricao, preco, quantidade, id)
+	defer db.Close()
 }
